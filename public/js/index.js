c691545eec61f7639748f879d3765e009afec812
Ron Berliner <uniliner@gmail.com>
2018-10-17 21:23:03 +0300

    add form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,21 @@ socket.on('connect', function() {
 socket.on('disconnect', function() {
   console.log('disconnected from server');
 });
-
 socket.on('newMessage', function(message) {
   console.log('new message', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'user',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
 });
--- server/server.js
+++ server/server.js
@@ -20,10 +20,11 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('message created: ', message);
 
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('this is from the server');
   });
 
   socket.on('disconnect', () => {
